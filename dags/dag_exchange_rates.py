e8e954400c9ea74563303fc910bc9b490e51a716
Alexander Sandoval <kevsanmm@gmail.com>
2024-09-15 11:46:56 -0500

    Actualización automática

--- dags/dag_exchange_rates.py
+++ dags/dag_exchange_rates.py
@@ -9,8 +9,9 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2023, 9, 14),
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'email': ['airflowdocker@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
